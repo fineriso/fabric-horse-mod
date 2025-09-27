f66f236aea70f26bd4c4b9bfce2fadf91c289bc7
fineriso <fin3riso@gmail.com>
2025-09-27 22:47:47 +0200

    initial commit

---
+++ src/main/java/net/fineriso/horsemod/Horsemod.java
@@ -0,0 +1,15 @@
+package net.fineriso.horsemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Horsemod implements ModInitializer {
+	public static final String MOD_ID = "horsemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
